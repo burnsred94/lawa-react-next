1e9ba317874a2c3e46df406097af38628bcb1352
Oleg Kamenshikov <burnsred94@gmail.com>
2023-03-15 16:05:02 +0300

    url add

--- strapi-backend/config/server.ts
+++ strapi-backend/config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '178.172.236.218'),
   port: env.int('PORT', 1337),
+  url: env('API_URL', 'http://178.172.236.218'),
   app: {
     keys: env.array('APP_KEYS'),
   },
