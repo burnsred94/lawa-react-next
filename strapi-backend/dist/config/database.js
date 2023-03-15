d621b3d7587f4af9ef1e92b43c9c4d59cece454f
Oleg Kamenshikov <burnsred94@gmail.com>
2023-03-15 07:57:40 +0300

    add dist

---
+++ strapi-backend/dist/config/database.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = ({ env }) => ({
+    connection: {
+        client: "postgres",
+        connection: {
+            host: env("DATABASE_HOST", "postgres"),
+            port: env.int("DATABASE_PORT", 50608),
+            database: env("DATABASE_NAME", "strapi"),
+            user: env("DATABASE_USERNAME", "strapi"),
+            password: env("DATABASE_PASSWORD", "strapi"),
+            schema: env("DATABASE_SCHEMA", "public"),
+            ssl: env("DATABASE_SSL", false),
+        },
+        debug: false,
+    },
+});
---
+++ strapi-backend/dist/src/api/our-work/controllers/our-work.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * our-work controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::our-work.our-work');
