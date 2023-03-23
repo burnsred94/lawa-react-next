7e2c90ca0eb5689e542c53ec0471a7eedd73de3e
Oleg Kamenshikov <burnsred94@gmail.com>
2023-03-23 23:18:51 +0300

    Add populate reviews-page

--- strapi-backend/src/api/reviews-page/controllers/reviews-page.ts
+++ strapi-backend/src/api/reviews-page/controllers/reviews-page.ts
@@ -2,6 +2,15 @@
  * reviews-page controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories, Strapi } from '@strapi/strapi'
+import { Context } from "koa"
 
-export default factories.createCoreController('api::reviews-page.reviews-page');
+export default factories.createCoreController('api::reviews-page.reviews-page', ({strapi}: {strapi: Strapi}) => ({
+
+  async find (ctx: Context) {
+    return await strapi.db.query('api::reviews-page.reviews-page').findMany({
+      populate: ['reviews.logo', 'seo.image']
+    });
+  }
+
+}));
