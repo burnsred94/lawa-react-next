dd247f302bf4f22f8d92eb97799d57a9e0cd4bc3
Oleg Kamenshikov <burnsred94@gmail.com>
2023-03-29 01:29:38 +0300

    Add sphere-page populate

--- strapi-backend/src/api/sphere-page/controllers/sphere-page.ts
+++ strapi-backend/src/api/sphere-page/controllers/sphere-page.ts
@@ -2,6 +2,23 @@
  * sphere-page controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories, Strapi } from '@strapi/strapi'
+import { Context } from "koa"
 
-export default factories.createCoreController('api::sphere-page.sphere-page');
+export default factories.createCoreController('api::sphere-page.sphere-page', ({ strapi }: { strapi: Strapi }) => ({
+
+  async find(ctx: Context) {
+    return await strapi.db.query('api::sphere-page.sphere-page').findMany({
+      populate: [
+        "images_header",
+        "services.preview_img",
+        "we_and_you.img",
+        "cases.image",
+        "reviews.logo",
+        "question",
+        "seo.image"
+
+      ]
+    })
+  }
+}));
