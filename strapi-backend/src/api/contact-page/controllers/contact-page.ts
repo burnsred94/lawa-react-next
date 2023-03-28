fc51687aee907a57ae578a59a86d37741f62f5d9
Oleg Kamenshikov <burnsred94@gmail.com>
2023-03-29 00:11:32 +0300

    add return in db contacts page

--- strapi-backend/src/api/contact-page/controllers/contact-page.ts
+++ strapi-backend/src/api/contact-page/controllers/contact-page.ts
@@ -2,6 +2,12 @@
  * contact-page controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories, Strapi } from '@strapi/strapi'
 
-export default factories.createCoreController('api::contact-page.contact-page');
+export default factories.createCoreController('api::contact-page.contact-page', ({ strapi }: { strapi: Strapi }) => ({
+
+  async find() {
+    return await strapi.db.query('api::contact-page.contact-page').findMany()
+  }
+
+}));
