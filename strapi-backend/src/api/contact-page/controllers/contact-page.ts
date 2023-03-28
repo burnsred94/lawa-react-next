67bbcbada30cbab221b5609c525a1ac484b8ef8c
Oleg Kamenshikov <burnsred94@gmail.com>
2023-03-29 00:15:12 +0300

    fix contacts

--- strapi-backend/src/api/contact-page/controllers/contact-page.ts
+++ strapi-backend/src/api/contact-page/controllers/contact-page.ts
@@ -3,11 +3,14 @@
  */
 
 import { factories, Strapi } from '@strapi/strapi'
+import { Context } from 'koa'
 
 export default factories.createCoreController('api::contact-page.contact-page', ({ strapi }: { strapi: Strapi }) => ({
 
-  async find() {
-    return await strapi.db.query('api::contact-page.contact-page').findMany()
+  async find(ctx: Context) {
+    return await strapi.db.query('api::contact-page.contact-page').findMany({
+      populate: ["question"]
+    })
   }
 
 }));
