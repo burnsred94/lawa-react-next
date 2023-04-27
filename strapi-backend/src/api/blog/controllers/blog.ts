532a87a9ab22c408b2818c4808a1b947e6deaa4b
Oleg Kamenshikov <burnsred94@gmail.com>
2023-04-27 23:16:30 +0300

    add populate blogs

--- strapi-backend/src/api/blog/controllers/blog.ts
+++ strapi-backend/src/api/blog/controllers/blog.ts
@@ -6,10 +6,29 @@ import { factories } from '@strapi/strapi';
 import { Strapi } from '@strapi/strapi';
 import { Context } from 'koa';
 
-export default factories.createCoreController('api::blog.blog', ({strapi}:{strapi: Strapi})=>({
-    async find(ctx: Context) {
-      return await strapi.db.query('api::blog.blog').findMany({
-        populate: ['seo.image', 'question', 'posts.img']
-      })
+export default factories.createCoreController('api::blog.blog', ({ strapi }: { strapi: Strapi }) => ({
+  async find(ctx: Context) {
+    const data = await strapi.db.query('api::blog.blog').findMany({
+      populate: ['seo.image', 'question', 'posts.img']
+    })
+
+    const dataPopulate = []
+
+    let dataSet = []
+
+    for (let i = 0; i < data.length; i++) {
+      dataSet.push(data[i])
+
+      if (dataSet.length === 9) {
+        dataPopulate.push(dataSet)
+        dataSet = []
+      }
     }
+
+    if (dataSet.length > 0) {
+      dataPopulate.push(dataSet)
+    }
+
+    return dataPopulate
+  }
 }));
